4f71ca9e0ff034c0c916bd018ad24d1947e57a28
Mintesinot <mintesinotteferamt@gmail.com>
2025-06-24 20:00:49 +0300

    email-password verification done

--- frontend/app/signin/page.jsx
+++ frontend/app/signin/page.jsx
@@ -24,7 +24,7 @@
 //     <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
 //       <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-6">
 //         <h1 className="text-2xl font-bold mb-6 text-center">Sign in to RoomieFinder</h1>
-        
+
 //         {/* Google Sign-In Button */}
 //         <button
 //           onClick={() => alert("Google Sign-In coming soon")} // Replace with real logic
@@ -54,6 +54,7 @@
 
 "use client";
 import AuthForm from "@/components/AuthForm";
+// import { signIn } from "next-auth/react";
 
 const SignInPage = () => {
   return (
@@ -78,21 +79,21 @@ const SignInPage = () => {
             Sign in to your RoomieFinder Account
           </h1>
 
-         {/* Google Sign-In Button */}
-        <button
-        // onClick={() => signIn("google")}
-          href="http://localhost:5000/api/auth/google"
+          {/* Google Sign-In Button */}
+          <button
+            // onClick={() => signIn("google")}
+            // href="http://localhost:5000/api/auth/google"
 
-          // onClick={() => alert("Google Sign-In coming soon")} // Replace with real logic
-          className="w-full flex items-center justify-center gap-3 text-sm font-medium border border-gray-300 rounded-md px-4 py-2 hover:bg-gray-100 mb-4"
-        >
-          <img
-            src="https://www.svgrepo.com/show/475656/google-color.svg"
-            alt="Google"
-            className="w-5 h-5"
-          />
-          Continue with Google
-        </button>
+            // onClick={() => alert("Google Sign-In coming soon")} // Replace with real logic
+            className="w-full flex items-center justify-center gap-3 text-sm font-medium border border-gray-300 rounded-md px-4 py-2 hover:bg-gray-100 mb-4"
+          >
+            <img
+              src="https://www.svgrepo.com/show/475656/google-color.svg"
+              alt="Google"
+              className="w-5 h-5"
+            />
+            Continue with Google
+          </button>
 
           <AuthForm type="signin" />
         </div>
